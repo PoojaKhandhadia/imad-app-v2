f7c3eed94068def33daaa1a72a393afc4f084dfa
Pooja Khandhadia <pooja.khandhadia.pk@gmail.com>
2017-02-26 21:27:54 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,10 @@ element.innerHTML="New Value";
 var img=document.getElementById("madi");
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = MarginLeft + 5;
+    marginLeft = MarginLeft + 10;
     img.style.marginLeft = marginLeft + "px";
 }
 img.onclick=function(){
-    var interval = setInterval(moveRight,50);
-};
+    var interval = setInterval(moveRight,100);
+    img.style.marginLeft = "100px";
+}
